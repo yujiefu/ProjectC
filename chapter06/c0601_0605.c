f17fa278a9c07f96b1887a4bd4fbf75b05412d55
yujiefu <yujie.fu@hotmail.com>
2020-05-17 01:33:29 +0800

    批量数据处理_数组

---
+++ chapter06/c0601_0605.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+//0601 函数返回最大值；
+//描述：设计一个函数，求三个整数的最大值。在主函数中给定三个整数，验证所设计函数的正确性。
+//输入形式：输入一行，依次为三个整数；
+//输出形式：输出一行，为三个数中的最大数；
+//样例输入：12 15 9
+//样例输出：15
+int max_value(int x,int y,int z)
+{
+	
+	int max=x;
+	if(y>max) max=y;
+	if(z>max) max=z;
+	
+	return max;	
+
+}
+
+int main()
+{
+	int a, b,c;
+	
+	scanf("%d%d%d",&a,&b,&c);
+
+	printf("%d",max_value(a,b,c));	
+	
+	return 0;
+} 
+*/
